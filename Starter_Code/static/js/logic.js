3b8a26b14196bf560f57c6acd3a095afb62ce912
Sam <dasamboni8@gmail.com>
2024-05-22 00:49:30 -0400

    cleaned logic.js

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -7,13 +7,6 @@ d3.json(queryURL).then(function (data) {
     makeFeatures(data.features);
 });
 
-//Making a hover-over popup menu for each earthquakes' location and time
-// function makeFeatures(earthquakeData) {
-//     function onEachFeature(feature, layer) {
-//         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>$
-//         {"Depth: " + feature.geometry.coordinates[2]} <p>${"Magnitude: " + feature.properties.mag}<hr>`)};
-    
-//     };
 
 function makeFeatures(earthquakeData){
         function onEachFeature(feature, layer) {
